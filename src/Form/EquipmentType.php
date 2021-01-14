e7603d876af5c42fe668a1096b7366f4c0e9ae8d
Morgane JUMEL <jumel.morgane@gmail.com>
2021-01-14 12:44:32 +0100

    Added localisation for User and Equipment forms

--- src/Form/EquipmentType.php
+++ src/Form/EquipmentType.php
@@ -3,6 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Equipment;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,17 +16,17 @@ class EquipmentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('registration')
-            ->add('brand')
-            ->add('type')
-            ->add('model')
-            ->add('registrationYear')
-            ->add('buyValue')
-            ->add('lifetime')
-            ->add('workTime')
-            ->add('horsepower')
-            ->add('useCost')
-            ->add('residualValue')
+            ->add('registration', TextType::class, ['label' => "Immatriculation"])
+            ->add('brand', TextType::class, ['label' => "Marque"])
+            ->add('type', TextType::class, ['label' => "Catégorie"])
+            ->add('model', TextType::class, ['label' => "Modèle"])
+            ->add('registrationYear', DateTimeType::class, ['label' => "Année"])
+            ->add('buyValue', IntegerType::class, ['label' => "Valeur d'achat"])
+            ->add('lifetime', IntegerType::class, ['label' => "Durée de vie"])
+            ->add('workTime', IntegerType::class, ['label' => "Heures de travail"])
+            ->add('horsepower', IntegerType::class, ['label' => "Puissance du véhicule"])
+            ->add('useCost', NumberType::class, ['label' => "Coût d'utilisation"])
+            ->add('residualValue', IntegerType::class, ['label' => "Valeur résiduelle"])
         ;
     }
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,14 +15,14 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('email')
-            ->add('password', PasswordType::class)
-            ->add('firstname')
-            ->add('lastname')
-            ->add('address')
-            ->add('siret')
-            ->add('phone')
+            ->add('pseudo', TextType::class, ['label' => "Pseudonyme"])
+            ->add('email', TextType::class, ['label' => "E-mail"])
+            ->add('password', PasswordType::class, ['label' => "Mot de passe"])
+            ->add('firstname', TextType::class, ['label' => "Prénom"])
+            ->add('lastname', TextType::class, ['label' => "Nom"])
+            ->add('address', TextType::class, ['label' => "Adresse"])
+            ->add('siret', IntegerType::class, ['label' => "SIRET"])
+            ->add('phone', TextType::class, ['label' => "Téléphone"])
         ;
     }
 
