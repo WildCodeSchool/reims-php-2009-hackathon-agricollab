6e1f299ed665c79a991a9937b2c9763e89a2e3d3
Vayssie-web <vayssie.web@gmail.com>
2021-01-14 14:37:05 +0100

    Feat session in EquipmentController, add simple.html.twig template

--- src/Controller/EquipmentController.php
+++ src/Controller/EquipmentController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 /**
  * @Route("/equipment")
@@ -22,6 +23,36 @@ class EquipmentController extends AbstractController
     {
         return $this->render('equipment/index.html.twig', [
             'equipment' => $equipmentRepository->findAll(),
+        ]);
+    }
+    /** 
+     * @Route("/simple", name="simple_equipment", methods={"GET"})
+     */
+    public function simpleEquipment(SessionInterface $session, Request $request): Response
+    {
+        $size = $request->query->get('size');
+        $age = $request->query->get('age');
+        $color = $request->query->get('color');
+        $usage = $request->query->get('usage');
+
+        if ($age != null) {
+            $session->set('age', $age);
+        }
+        if ($size != null) {
+            $session->set('size', $size);
+        }
+        if ($color != null) {
+            $session->set('color', $color);
+        }
+        if ($usage != null) {
+            $session->set('usage', $usage);
+        }
+
+        return $this->render('equipment/simple.html.twig', [
+            'size'=>$session->get('size'),
+            'age'=>$session->get('age'),
+            'color'=>$session->get('color'),
+            'usage'=>$session->get('usage')
         ]);
     }
 
